367fd6e405c65d649b1b4ec9dc03896d011c70da
Manuel Núñez <manuel.nunez@hawkore.com>
2019-01-26 21:00:12 +0100

    Add mixin to ignore ignite types on JSON serialization

---
+++ example-mule3-ignite-connector/src/main/java/com/hawkore/ignite/connector/examples/MixInForIgnoreType.java
@@ -0,0 +1,14 @@
+package com.hawkore.ignite.connector.examples;
+
+import org.codehaus.jackson.annotate.JsonIgnoreType;
+
+/**
+ * 
+ * Used to ignore some classes when using JSON serialization
+ *
+ * @author Manuel Núñez (manuel.nunez@hawkore.com)
+ * 
+ */
+@JsonIgnoreType
+public class MixInForIgnoreType {
+}
