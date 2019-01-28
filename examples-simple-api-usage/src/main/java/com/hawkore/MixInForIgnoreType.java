2a56d7d77c770e377537790ad7cbecbf350d2679
Manuel Núñez <manuel.nunez@hawkore.com>
2019-01-28 12:44:27 +0100

    Add Mixin to json:object-to-json-transformer
    Ignore ignite artifacts on wrappers when use JSON serialization

---
+++ examples-simple-api-usage/src/main/java/com/hawkore/MixInForIgnoreType.java
@@ -0,0 +1,16 @@
+package com.hawkore;
+
+import org.codehaus.jackson.annotate.JsonIgnoreType;
+
+/**
+ * 
+ * Mixin (json:serialization-mixin) to ignore some classes when using JSON
+ * serialization (json:object-to-json-transformer)
+ *
+ * @author Manuel Núñez (manuel.nunez@hawkore.com)
+ * 
+ */
+@JsonIgnoreType
+public class MixInForIgnoreType {
+
+}
